3dd92ffa874c947dd01fbeab3346c2f488b03f4c
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-06-25 14:25:04 -0300

    Binary Tree Insert

---
+++ binary_tree_insert/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	insert(root, val)
+	return root
+}
+
+func insert(root *TreeNode, val int) {
+	if root.Val > val {
+		if root.Left == nil {
+			root.Left = &TreeNode{Val: val}
+		} else {
+			insert(root.Left, val)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = &TreeNode{Val: val}
+		} else {
+			insert(root.Right, val)
+		}
+	}
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
